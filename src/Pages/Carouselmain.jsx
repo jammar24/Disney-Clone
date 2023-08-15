a97483e68c787682d67f6b1bce355e508926fc97
jammar24 <jamarodalis@yahoo.com>
2023-08-14 22:41:42 -0500

    commit carouselmain

---
+++ src/Pages/Carouselmain.jsx
@@ -0,0 +1,21 @@
+import 'react'
+import '../Pages/styles/Carouselmain.css'
+const Carouselmain = () => {
+  return (
+            <div className="slide-content">
+              
+       <img className='slide__img' src="https://raw.githubusercontent.com/kunaal438/disney-plus-clone/master/images/slider%203.PNG"  alt="" />
+
+       <div className="owl-dots">
+        <button role="button" className="owl-dot"><span></span>
+        </button><button role="button" className="owl-dot"><span></span></button>
+        <button role="button" className="owl-dot active"><span></span></button>
+          <button role="button" className="owl-dot"><span></span></button>
+          <button role="button" className="owl-dot"><span></span></button>
+          </div>
+         </div>
+
+  )
+}
+
+export default Carouselmain
--- src/Pages/Disney.jsx
+++ src/Pages/Disney.jsx
@@ -2,11 +2,13 @@ import  'react'
 import Navbar from './Navbar'
 import Categories from './Categories'
 import Footer from './Footer'
+import Carouselmain from './Carouselmain'
 
 const Disney = () => {
   return (
     <>
     <Navbar/>
+    <Carouselmain/>
     <Categories/>
     <Footer/>
     </>
