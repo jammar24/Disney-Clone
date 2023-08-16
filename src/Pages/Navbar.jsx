434e3b9224247561993da6075a22e313071bc5bb
jammar24 <jamarodalis@yahoo.com>
2023-08-16 09:46:11 -0500

    dependencies, installations, correction of the navbar

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,15 +1,23 @@
 import 'react';
 import './styles/navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from "react-redux";
 import logoDsny from '../assets/dny.png'
 import ft from '../assets/ft.jpg'
+import { useState } from 'react';
 
 const Navbar = () => {
   const { nameUser} = useSelector((state) => state);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
-    <nav className='navbar'>
+    <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>      
         <img src= { logoDsny }   className='logop' alt="logo" />
-        <ul  className='nav__ln'>
+        <ul className={`nav__ln ${isMenuOpen ? 'open' : ''}`}>
             <li className='nav__itm'><i className='bx bxs-home'></i><a href="#">home</a></li>
             <li className='nav__itm' ><i className='bx bx-search'></i><a href="#">search</a></li>
             <li className='nav__itm'  ><i className='bx bx-plus-medical'></i><a href="#">wachtlist</a></li>
@@ -21,6 +29,9 @@ const Navbar = () => {
         <p className='nav__nameuser'>{ nameUser }</p>
         <img className='nav__ft' src={ft}  alt="foto de auth" />
         </div>
+        <div className={`nav__menu-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
     </nav>
   )
 }
