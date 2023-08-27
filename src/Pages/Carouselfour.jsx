0ace44937994660dc6342c5efa59acb7ca4d636f
jammar24 <jamarodalis@yahoo.com>
2023-08-27 18:03:35 -0500

    data movies

--- src/Pages/Carouselfour.jsx
+++ src/Pages/Carouselfour.jsx
@@ -3,12 +3,12 @@ import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import dataDocumental from "../helpers/datadocumental";
-import '../Pages/styles/carouselsecond.css'
+import '../Pages/styles/carouselfour.css'
 const Carouselfour = () => {
   return (
     <>
     <div>
-      <h2 id="movie"> Documentales </h2>
+      <h2 id="documental"> Documentales </h2>
     </div>
       <OwlCarousel
         className="owl-theme"
@@ -20,9 +20,9 @@ const Carouselfour = () => {
        
         {dataDocumental.map((data) => (
           <>
-            <article key={data.imagen} className={` car ${data.imagen}`}>
-              <img className="img" src={data.imagen}  /> 
-             <button className="list-btn">{data.agregar_a_lista}</button>
+            <article key={data.imagen} className={` car-two ${data.imagen}`}>
+              <img className="img-two" src={data.imagen}  /> 
+             <button className="list-btn-two">{data.agregar_a_lista}</button>
             </article>
           </>
         ))}
--- src/Pages/Carouselthird.jsx
+++ src/Pages/Carouselthird.jsx
@@ -3,13 +3,13 @@ import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import dataSeries from "../helpers/dataseries";
-import '../Pages/styles/carouselsecond.css'
+import '../Pages/styles/carouselthird.css'
 
 const Carouselthird = () => {
   return (
     <>
     <div>
-      <h2 id="movie"> Series </h2>
+      <h2 id="series"> Series </h2>
     </div>
       <OwlCarousel
         className="owl-theme"
@@ -21,9 +21,9 @@ const Carouselthird = () => {
        
         {dataSeries.map((data) => (
           <>
-            <article key={data.imagen} className={` car ${data.imagen}`}>
-              <img className="img" src={data.imagen}  /> 
-             <button className="list-btn">{data.agregar_a_lista}</button>
+            <article key={data.imagen} className={` car-uno ${data.imagen}`}>
+              <img className="img-uno" src={data.imagen}  /> 
+             <button className="list-btn-uno">{data.agregar_a_lista}</button>
             </article>
           </>
         ))}
