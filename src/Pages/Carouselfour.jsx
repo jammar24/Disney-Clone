0ce476b4ad0759f96eeb38c41cd7a3b5fae00fa3
jammar24 <jamarodalis@yahoo.com>
2023-09-15 22:59:56 -0500

    test

--- src/Pages/Carouselfour.jsx
+++ src/Pages/Carouselfour.jsx
@@ -8,13 +8,26 @@ const Carouselfour = () => {
   return (
     <>
     <div>
-      <h2 id="documental">For You </h2>
+      <h2 id="documental"> Recommended For You  </h2>
     </div>
       <OwlCarousel
         className="owl-theme"
         dotsContainer= "false"
         items={6}
         loop
+        responsive={{
+          0: {
+            items: 1, // En pantallas pequeñas, muestra 1 elemento a la vez
+          },
+          600: {
+            items: 3, // En pantallas medianas, muestra 3 elementos a la vez
+          },
+          1000: {
+            items: 6, // En pantallas grandes, muestra 6 elementos a la vez
+          },
+        }}
+      
+        margin={37}
         autoplay
         autoplayTimeout={15000}
       >
--- src/helpers/datadocumental.jsx
+++ src/helpers/datadocumental.jsx
@@ -1,33 +1,81 @@
 const dataDocumental = [
     {
       id: 1,
-      nombre: "Nombre de la Película ",
+      nombre: "diario de una princesa",
       imagen:
-        "https://raw.githubusercontent.com/kunaal438/disney-plus-clone/master/images/poster%201.png",
+        "https://i.pinimg.com/564x/a3/88/2b/a3882bd038326a47ff3129d032647bb1.jpg",
       descripcion: "Lorem ipsum dolor sit amet consectetur.",
       agregar_a_lista: "add to watchlist",
     },
     {
       id: 2,
-      nombre: "Nombre de la Película ",
+      nombre: "toy story ",
       imagen:
-        "https://raw.githubusercontent.com/kunaal438/disney-plus-clone/master/images/poster%201.png",
+        "https://i.pinimg.com/236x/e5/2b/be/e52bbeb0b948a0c4055495bde9430ac7.jpg",
       descripcion: "Lorem ipsum dolor sit amet consectetur.",
       agregar_a_lista: "add to watchlist",
     },
     {
       id: 3,
-      nombre: "Nombre de la Película ",
+      nombre: "zack and cody",
       imagen:
-        "https://raw.githubusercontent.com/kunaal438/disney-plus-clone/master/images/poster%201.png",
+        "https://i.pinimg.com/236x/c4/63/29/c46329a6a2f997f84aafaaf03965d80b.jpg",
       descripcion: "Lorem ipsum dolor sit amet consectetur.",
       agregar_a_lista: "add to watchlist",
     },
     {
       id: 4,
-      nombre: "Nombre de la Película ",
+      nombre: "cinderella",
       imagen:
-        "https://raw.githubusercontent.com/kunaal438/disney-plus-clone/master/images/poster%201.png",
+        "https://i.pinimg.com/236x/75/50/8d/75508d6d89c19ea741a63b5418ccb1ec.jpg",
+      descripcion: "Lorem ipsum dolor sit amet consectetur.",
+      agregar_a_lista: "add to watchlist",
+    },
+    {
+      id: 5,
+      nombre: "hocus pocus",
+      imagen:
+        "https://i.pinimg.com/236x/b0/2a/2a/b02a2a944835c206f4c8c972cbd5ea4e.jpg",
+      descripcion: "Lorem ipsum dolor sit amet consectetur.",
+      agregar_a_lista: "add to watchlist",
+    },
+    {
+      id: 6,
+      nombre: "boy",
+      imagen:
+        "https://i.pinimg.com/236x/39/97/ce/3997ce53d305b6e9b88a80abb721761e.jpg",
+      descripcion: "Lorem ipsum dolor sit amet consectetur.",
+      agregar_a_lista: "add to watchlist",
+    },
+    {
+      id: 7,
+      nombre: "juego de gemelas",
+      imagen:
+        "https://i.pinimg.com/236x/ef/b2/1a/efb21ad723f5f1257b7be54a03cc6c75.jpg",
+      descripcion: "Lorem ipsum dolor sit amet consectetur.",
+      agregar_a_lista: "add to watchlist",
+    },
+    {
+      id: 8,
+      nombre: "sirenita",
+      imagen:
+        "https://i.pinimg.com/236x/82/f1/d7/82f1d7c4e66164f0754f589974c816d7.jpg",
+      descripcion: "Lorem ipsum dolor sit amet consectetur.",
+      agregar_a_lista: "add to watchlist",
+    },
+    {
+      id: 9,
+      nombre: "mommi",
+      imagen:
+        "https://i.pinimg.com/236x/5c/cf/68/5ccf687d53c559235501e221bc21d39d.jpg",
+      descripcion: "Lorem ipsum dolor sit amet consectetur.",
+      agregar_a_lista: "add to watchlist",
+    },
+    {
+      id: 10,
+      nombre: "lizzie",
+      imagen:
+        "https://i.pinimg.com/236x/a8/e2/5a/a8e25adca6e41080aa9a88b9c6b37ac6.jpg",
       descripcion: "Lorem ipsum dolor sit amet consectetur.",
       agregar_a_lista: "add to watchlist",
     },
